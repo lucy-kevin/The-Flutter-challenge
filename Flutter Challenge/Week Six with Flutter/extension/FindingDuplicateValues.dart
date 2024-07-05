88ddb23a088ea2fe56d43bf67913dcedbead0c92
lucy-kevin <kayikayikevin7@gmail.com>
2024-07-05 21:32:01 +0300

    finally

--- Flutter Challenge/Week Six with Flutter/extension/FindingDuplicateValues.dart
+++ Flutter Challenge/Week Six with Flutter/extension/FindingDuplicateValues.dart
@@ -1,6 +1,7 @@
 void main(List<String> args) {
   print([1,2,3,4,5,4].containDuplicated);
   print([1,2,3,4,5].containDuplicated);
+  print(["Kevin", "Julie", "Kevin"].containDuplicated);
   
 }
 extension on Iterable{
---
+++ Flutter Challenge/Week Six with Flutter/extension/finding&mappingKey&valuesOnAMap.dart
@@ -0,0 +1,22 @@
+const json ={
+  'name' : 'Foo Bar',
+  'age' : 20,
+};
+void main(List<String> args) {
+  final String? ageAsString = json.find<int>("age", (int age) => age.toString(),);
+  print(ageAsString);
+  
+}
+extension Find<K, V, R> on Map<K, V>{
+  R? find<T>(
+    K key,
+    R? Function(T value) cast,
+  ){
+    final value = this[key];
+    if(value != null && value is T){
+      return cast(value as T);
+    }else{
+      return null;
+    }
+  }
+}
