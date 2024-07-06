ca5941b6d6a8eeadb4dcd8a00e9640b19cbf00b7
lucy-kevin <kayikayikevin7@gmail.com>
2024-07-06 11:41:02 +0300

    something small

--- Flutter Challenge/Week Six with Flutter/extension/finding&mappingKey&valuesOnAMap.dart
+++ Flutter Challenge/Week Six with Flutter/extension/finding&mappingKey&valuesOnAMap.dart
@@ -5,7 +5,8 @@ const json ={
 void main(List<String> args) {
   final String? ageAsString = json.find<int>("age", (int age) => age.toString(),);
   print(ageAsString);
-  
+  final String? name = json.find("name", (String name) => name.toString(),);
+  print(name);
 }
 extension Find<K, V, R> on Map<K, V>{
   R? find<T>(
