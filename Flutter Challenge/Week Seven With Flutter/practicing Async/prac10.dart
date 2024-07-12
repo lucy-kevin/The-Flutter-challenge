bb19d002f195a8b7015461156a5080399d60b891
lucy-kevin <kayikayikevin7@gmail.com>
2024-07-12 18:48:24 +0300

    broadcasting

--- Flutter Challenge/Week Seven With Flutter/practicing Async/prac10.dart
+++ Flutter Challenge/Week Seven With Flutter/practicing Async/prac10.dart
@@ -1,3 +1,29 @@
-void main(List<String> args) {
+import 'dart:async';
+
+void main(List<String> args) async {
+  await BroadcastingNames();
   
+}
+Future<void> BroadcastingNames() async{
+  final StreamController<String> controller;
+
+  controller = StreamController<String>.broadcast();
+  final sub1 = controller.stream.listen((name) {
+    print("sub1: $name");
+  });
+  final sub2 = controller.stream.listen((name) {
+    print("sub2: $name");
+  });
+  controller.sink.add("Kevin");
+  controller.sink.add("Julie");
+  controller.close();
+
+  controller.onCancel =() {
+    
+    print("onCancel");
+    sub2.cancel();
+    sub2.cancel();
+    sub1.cancel();
+    sub1.cancel();
+  };
 }
