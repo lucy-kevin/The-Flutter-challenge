545e8a6975b8b9b9ffa1c19d7121b582e3422d72
lucy-kevin <kayikayikevin7@gmail.com>
2024-07-20 12:56:33 +0300

    pushing

--- Projects/mynotes/lib/main.dart
+++ Projects/mynotes/lib/main.dart
@@ -11,8 +11,29 @@ void main() {
       home: const HomePage(),
     ));
 }
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage ({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  void initState(){
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +41,22 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title:const Text("Register"),
         ),
-        body: TextButton(
-          onPressed: () {
+        body: Column(
+          children: [
+            TextField(
+              controller: _email,
 
-          }, child: Text("Hello"),
-
+            ),
+            TextField(
+              controller: _password,
+            ),
+            TextButton(
+              onPressed: () async{
+                
+              }, 
+              child: const Text("Register"),          
+            ),
+          ],
         )
     );
   }
