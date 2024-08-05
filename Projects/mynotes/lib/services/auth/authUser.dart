94637f89eb1cacd7e3a8106ddc3e92d75f898885
lucy-kevin <kayikayikevin7@gmail.com>
2024-08-05 23:18:29 +0300

    crud

--- Projects/mynotes/lib/services/auth/authUser.dart
+++ Projects/mynotes/lib/services/auth/authUser.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 @immutable //it can not have fields that chanfe let it be it sub class
 class AuthUser{
   final bool isEmailVerified;
-  AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
 
-  factory AuthUser.fromFirebase(User user)=> AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user)=> 
+  AuthUser(isEmailVerified: user.emailVerified);
+
+
 }
