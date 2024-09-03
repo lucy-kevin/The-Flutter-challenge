0c156427a459eae03c6a5f3c3db1fa19d06b8b43
lucy-kevin <kayikayikevin7@gmail.com>
2024-09-03 18:09:54 +0300

    flutter

--- Flutter Challenge/ticket_app/lib/screens/profiles/profiles.dart
+++ Flutter Challenge/ticket_app/lib/screens/profiles/profiles.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/widgets/heading_text.dart';
+import 'package:ticket_app/base/widgets/text_style_forth.dart';
+import 'package:ticket_app/base/widgets/text_style_third.dart';
 
 class ProfilesScreen extends StatelessWidget {
   const ProfilesScreen({super.key});
@@ -12,17 +14,20 @@ class ProfilesScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppStyles.bgColor,
       body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
         children: [
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Padding(padding: EdgeInsets.only(top: 160)),
+              const Padding(padding: EdgeInsets.only(top: 40)),
+
               //shows the logo
               Container(
                 width: 86,
                 height: 86,
                 decoration: BoxDecoration(
-                    image: DecorationImage(image: AssetImage(AppMedia.logo)),
+                    image:
+                        const DecorationImage(image: AssetImage(AppMedia.logo)),
                     borderRadius: BorderRadius.circular(10)),
               ),
               //shows th column text
@@ -44,7 +49,7 @@ class ProfilesScreen extends StatelessWidget {
                         color: Colors.grey.shade500),
                   ),
                   const SizedBox(
-                    height: 8,
+                    height: 4,
                   ),
                   Container(
                     padding: EdgeInsets.symmetric(horizontal: 3, vertical: 3),
@@ -80,7 +85,68 @@ class ProfilesScreen extends StatelessWidget {
                     ),
                   ),
                 ],
-              )
+              ),
+              Expanded(child: Container()),
+              Text(
+                "Edit",
+                style: TextStyle(
+                  color: AppStyles.primaryColor,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
+          Divider(color: Colors.grey.shade300),
+          Stack(
+            children: [
+              Container(
+                height: 90,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: AppStyles.primaryColor,
+                    borderRadius: BorderRadius.circular(18)),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      maxRadius: 25,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        FluentSystemIcons.ic_fluent_lightbulb_filament_filled,
+                        color: AppStyles.primaryColor,
+                        size: 28,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextStyleThird(text: "You'v got a new award "),
+                        Text(
+                          "You have 95 flights in a year",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white.withOpacity(0.5)),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                  top: -25,
+                  right: -25,
+                  child: Container(
+                    padding: EdgeInsets.all(23),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border:
+                            Border.all(width: 17, color: Color(0xff264cd2))),
+                  ))
             ],
           )
         ],
